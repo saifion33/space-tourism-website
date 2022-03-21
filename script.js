49e7bdb903b9850ebffd89383c7078a2377a60b8
saifion33 <mxofficialplayer@gmail.com>
2022-03-21 16:01:52 +0530

    change in script and style and page content folder rename to page template

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 "use strict"
+// This javascript file part of space tourism website
+
+// Developer:- Azeem Saifi
+// Instagram:- www.instagram.com/frontendium
+
+
 // ############################################## DESKTOP NAVBAR RELATED FUNCTIONS #######################################
 
 const navLinksCollection = document.getElementsByClassName('nav-links');
@@ -33,7 +39,7 @@ function pageChanger(htmlData, clickedLinkId) {
 // This function fetch html data of page according clicked navbar link and give html data to page changer function
 function mainContentFetcher(clickedLinkId) {
     if (clickedLinkId != "") {
-        fetch(`./page-content/${clickedLinkId}-page.txt`).then(response => response.text()).then(data => pageChanger(data, clickedLinkId)).catch(err => console.log(err))
+        fetch(`./page-template/${clickedLinkId}-page.txt`).then(response => response.text()).then(data => pageChanger(data, clickedLinkId)).catch(err => console.log(err))
     }
 }
 // Execute this function initially to fetch home page content on load web page
@@ -49,71 +55,112 @@ const menuIcon = document.getElementById('menu-icon');
 const mobileMenu = document.getElementById('nav-link-primary-container');
 // Grab close icon to add event listner
 const closeMenuIcon = document.getElementById('close-menu-icon');
-
-// This function show mobile navbar
+// Grab body for add event listener
+const body = document.getElementById('body');
+// This function check if user clicked outside mobile navigation menu then it call menu hider function for hide menu
+let navbarOutClicklistener = function (event) {
+    // checking if user not clicked inside navigation menu on it's childern element or menu icon which used for show menu
+    if (!mobileMenu.contains(event.target) && event.target.id != 'menu-icon') {
+        menuHider()
+    }
+}
+// This function show mobile navbar and add a click event listener on body
 function menuDisplayer() {
     // hide hamburger icon
-    menuIcon.style.display = 'none';
+    menuIcon.style.display = 'none'
     // translate or show mobile navbar
     mobileMenu.style.transform = 'translateX(0%)';
+    // add a click a event listner for detect outside click of mobile menu
+    body.addEventListener('click', navbarOutClicklistener);
 }
-// This function hide navbar
+// This function hide navbar and remove click event listner from body
 function menuHider() {
     // hide navbar
     mobileMenu.style.transform = 'translateX(100%)';
     // show hamburger menu
     menuIcon.style.display = 'block';
+    // remove event listener
+    body.removeEventListener('click',navbarOutClicklistener);
 }
 
 // Add click event listner on hamburger icon and call menuDisplayer function
 menuIcon.addEventListener('click', () => menuDisplayer());
 // Add click event listner on close icon and call menuHider function
-closeMenuIcon.addEventListener('click', () => menuHider())
+closeMenuIcon.addEventListener('click', () => menuHider());
+// Add click event listner on mobile navbar to detect outside click of navbar
 
-// ************************************************************************************************************
+/*###################################################################################################################
+###############################------------  MAIN FUNCTIONS ------------------------#################################
+####################################################################################################################*/
+
+// **************************** PAGE SPECIFIC LINK ACTIVATER FUNCTION **********************************************
+
+// set active page specific navbar link id initially to 0 it changed by pageNavLinkActivater function.
 let activePageLink = '0';
+
+// This function use to present active page specific navbar links it takes page navbar clicked link id and page name
 function pageNavLinkActivater(linkId, pageName) {
-    // console.log('page activater called');
     document.getElementById(activePageLink).classList.remove(`active-${pageName}-link`);
     document.getElementById(linkId).classList.add(`active-${pageName}-link`);
-    console.log(activePageLink);
     activePageLink = linkId;
-    console.log(activePageLink);
 }
-function pageContentChanger(data,clickedLinkId, pageName, optionalArgs) {
-    pageNavLinkActivater(clickedLinkId,pageName)
+// *************************************** PAGE CONTENT CHANGER FUNCTION ********************************************
+
+// This function takes filtered data, main navbar clicked link id and page specific arguments and apply them in page
+function pageContentChanger(data, clickedLinkId, pageName, pageSpecificArgs) {
+    pageNavLinkActivater(clickedLinkId, pageName)
     // Grab page image 
     const image = document.getElementById(`${pageName}-image`);
     // Grab page content name Like moon,mars,launch vehical etc.
     const name = document.getElementById(`${pageName}-name`);
-    if (pageName=='technology') {
-        image.setAttribute('src', data.images.portrait);
+
+    /* check pageName because technology page images saved as portrait and landscape
+    other page image saved as png and webp name in data.json file */
+    if (pageName == 'technology') {
+        if (screen.width>800) {
+            image.setAttribute('src',data.images.portrait)
+        }
+        else {
+            image.setAttribute('src',data.images.landscape)
+        }
     }
     else {
-        image.setAttribute('src',data.images.webp)
+        // set image path
+        image.setAttribute('src', data.images.webp)
     }
+    // change name
     name.innerText = data.name;
-    optionalArgs.forEach((element) => {
-        document.getElementById(`${pageName}-${element}`).innerText = data[element];
-})
-    // name.innerText = data[name];
-}
-// Fetch page content according to clicked link and send filtered data to pageContentChanger function
 
-function pageContentFetcher(pageName, clickedLinkId, optionalArgs) {
-    console.log('page content fetcher called');
+    //Apply a for each loop on page specific Argument
+    pageSpecificArgs.forEach((element) => {
+        // get page specific argument one by one and chnage them from data
+        document.getElementById(`${pageName}-${element}`).innerText = data[element];
+    })
+}
+
+
+// ********************************************** PAGE CONTENT FETCHER FUNCTION ******************************************
+
+// Fetch page content from data.json file and filter it according to clicked link and send it to pageContentChanger function
+function pageContentFetcher(pageName, clickedLinkId, pageSpecificArgs) {
     fetch('./data.json').then(response => response.json()).then(data => {
-        pageContentChanger(data[pageName][clickedLinkId],clickedLinkId, pageName, optionalArgs);
+        // filter data according to page
+        const filteredData = data[pageName][clickedLinkId];
+        // send data to page content changer function
+        pageContentChanger(filteredData, clickedLinkId, pageName, pageSpecificArgs);
     }).catch(err => console.log(err));
 }
+
+
 
 /***************************************************************************************************************
 ###################################### PAGE SPECIFIC FUNCTIONS #################################################
 ****************************************************************************************************************/
 
-// ********************************** HOME FUNCTION **********************************************************1
+// ********************************** HOME FUNCTION **********************************************************
+// There is no need of this function this function for counter a small problem
 function home() {
-    console.log('home called')
+    console.log('home function called')
 }
 
 // ********************************** DESTINATIONS FUNCTION ****************************************************
@@ -132,9 +179,9 @@ function destinations() {
 
 // ************************************ CREW FUNCTION **********************************************************
 function crew() {
-     // Grab destinations page navbar
+    // Grab destinations page navbar
     const crewNavbarHtmlCollection = document.getElementsByClassName('crew-nav-links');
-     // convert html collection to array for run forEach loop
+    // convert html collection to array for run forEach loop
     const crewNavLinks = Array.from(crewNavbarHtmlCollection);
     // Execute pageContentFetcheer function initially for fetch first page content
     pageContentFetcher('crew', '0', ['role', 'bio']);
@@ -155,6 +202,6 @@ function technology() {
     // Add click event listener on page's navigation bar and when clicked call pageContentFetcher function 
     technologyNavLinks.forEach((technologyLink) => {
         document.getElementById(technologyLink.id).addEventListener('click', (event) => pageContentFetcher('technology', technologyLink.id, ['description']));
-    
+
     })
 }
